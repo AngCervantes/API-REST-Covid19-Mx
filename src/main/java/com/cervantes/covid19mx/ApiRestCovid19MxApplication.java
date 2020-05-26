0b34df260bb7f8470a92561635a2a0837f32b805
Ángel Cervantes <thedirtymac@live.com.mx>
2020-05-26 17:34:57 -0500

    first commit, project empty

---
+++ src/main/java/com/cervantes/covid19mx/ApiRestCovid19MxApplication.java
@@ -0,0 +1,13 @@
+package com.cervantes.covid19mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCovid19MxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCovid19MxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cervantes/covid19mx/ApiRestCovid19MxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cervantes.covid19mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCovid19MxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
